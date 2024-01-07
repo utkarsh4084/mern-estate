e2ea81a207fd1b48a10503ccb6849db5f33288ad
utkarsh4084 <gps1991@gmail.com>
2024-01-07 08:56:36 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {persistor, store} from './app/store.js'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App/>
+    </PersistGate>
+  </Provider>
 )
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,71 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { signInStart, signInSuccess, signInFailure } from "../app/user/userSlice";
+import { useDispatch, useSelector } from "react-redux"; 
 
 export default function SignIn() {
-    return (
-        <h1>Sign In 2</h1>
-    )
+  const [formData, setFormData] = React.useState({});
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {loading, error} = useSelector(state => state.user);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    dispatch(signInStart())
+    const res = await fetch('/api/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+    });
+
+    const data = await res.json();
+    
+    if(data.success === false) {
+      dispatch(signInFailure(data.message))
+      return
+    }
+    dispatch(signInSuccess(data));
+    navigate("/"); 
+  }
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input type='email' 
+              placeholder="email" 
+              className="border p-3 rounded-lg" 
+              id="email"
+              onChange={handleChange}/>
+        <input type='password' 
+              placeholder="password" 
+              className="border p-3 rounded-lg" 
+              id="password"
+              onChange={handleChange}/>
+        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg 
+                            uppercase hover:opacity-95 disabled:opacity-80">
+                {loading ? 'Loading...' : 'Sign In'}
+        </button>
+      </form>
+
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-700">Sign Up</span>
+        </Link> 
+      </div>
+      <div className="mt-5 text-red-700">{error}</div>
+    </div>
+  )
 }
