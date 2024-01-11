795eb02611c4f28c649b57132dd492d9083748aa
utkarsh4084 <gps1991@gmail.com>
2024-01-11 11:11:44 +0530

    update tp prev commit

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const test = (req, res) => {
     res.json({message: "Hello World"})
